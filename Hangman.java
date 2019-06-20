e623531ebde4485a97769c7abcf8d01ff5e549cc
debesh26360 <51988668+debesh26360@users.noreply.github.com>
2019-06-20 23:49:50 +0530

    Create Hangman.java

---
+++ Hangman.java
@@ -0,0 +1,27 @@
+Write any of the two codes of the main()
+
+1.
+package game1;
+
+public class hangman 
+{
+    public static void main(String[] args) 
+    {     
+        GameWindow gobj=new GameWindow();
+        gobj.addComponent();
+    }
+}
+
+2.
+
+package game1;
+
+public class hangman 
+{
+    public static void main(String[] args) 
+    {
+        Menu mobj=new Menu();
+        mobj.addcomponent();
+    }
+}
+
